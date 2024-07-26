fe6e69be5e3319ce511b75b12c74b3ed5c319326
umarali7270 <umaraliismoilov3101@gmail.com>
2024-07-26 11:47:44 +0500

    121

--- src/Header.jsx
+++ src/Header.jsx
@@ -34,8 +34,8 @@ export default function Header() {
                 </a>
 
                 <div style={nav ? {left: "-100%"} : {left: "0"}} className="md:hidden fixed top-0 pt-10  bg-[#323232b0] backdrop-blur-md transition-all left-0 block h-full w-full">
-                    <button  onClick={durbek}  className=" absolute top-4 right-0 text-5xl flex justify-end pr-5">
-                    <svg className='w-8 hamburger-x' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
+                    <button  onClick={durbek}  className=" absolute top-6 right-0 text-5xl flex justify-end pr-5">
+                    <svg className='w-4 hamburger-x' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
                     </button>
                 <ul className="md:hidden flex flex-col items-center mt-20 justify-center gap-10">
                     <li className=""><a href="#" className="font-bold text-lg text-[#DEDEDE] hover:text-[#9D9D9D] transition-all">Home</a></li>
