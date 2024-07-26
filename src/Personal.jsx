fd41a5bf2a3c5ecdf8e08911a8bbb5825aeb5675
umarali7270 <umaraliismoilov3101@gmail.com>
2024-07-26 11:43:25 +0500

    30

--- src/Personal.jsx
+++ src/Personal.jsx
@@ -12,7 +12,7 @@ export default function Personal() {
         <section className="md:pt-[120px] md:pb-[120px]  pt-14 pb-[100px] bg-[#202020] px-5">
             <div className="max-w-[1250px] mx-auto">
                 <h2 className="font-bold md:text-[36px] md:leading-[49px] text-center text-[28px] leading-[38px] mb-[34px] text-[#DEDEDE] md:mb-[65px]">Personal Projects</h2>
-                <div className="md:grid md:grid-cols-3 flex flex-col md:gap-6 gap-[12px] items-center">
+                <div className="md:grid md:grid-cols-3 flex flex-col md:gap-6 gap-6 items-center">
                     <div className="w-full h-full  overflow-hidden">
                       <img src={PersonalImg} alt="" className="flex rounded box-card w-full md:h-full h-[400px]" />
                     </div>
