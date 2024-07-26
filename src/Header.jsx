fe005385980efa67aefedcb5685483a5a8b51ed1
umarali7270 <umaraliismoilov3101@gmail.com>
2024-07-26 11:52:20 +0500

    00

--- src/Header.jsx
+++ src/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
                     </svg>
                 </a>
 
-                <div style={nav ? {left: "-100%"} : {left: "0"}} className="md:hidden fixed top-0 pt-10  bg-[#323232b0] backdrop-blur-md transition-all left-0 block h-full w-full">
+                <div style={nav ? {right: "-100%"} : {right: "0"}} className="md:hidden fixed top-0 pt-10  bg-[#323232b0] backdrop-blur-md transition-all right-0 block h-full w-[85%]">
                     <button  onClick={durbek}  className=" absolute top-6 right-0 text-5xl flex justify-end pr-5">
                     <svg className='w-4 hamburger-x' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
                     </button>
