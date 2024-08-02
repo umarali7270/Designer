9928091e792ab9dc6f070b3e5b1f15f702f73e72
umarali7270 <umaraliismoilov3101@gmail.com>
2024-08-02 10:07:18 +0500

    zzz

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -12,24 +12,24 @@ export default function Visual() {
         <section className="md:pt-[120px] md:pb-[120px]  pt-14 pb-[100px] bg-[#202020] px-5">
             <div className="max-w-[1250px] mx-auto">
                 <h2 className="font-bold md:text-[36px] md:leading-[49px] text-center text-[28px] leading-[38px] mb-[34px] text-[#DEDEDE] md:mb-[65px]">Visual Explorations</h2>
-                <div className="md:grid md:grid-cols-3 flex flex-col md:gap-6 gap-4 items-center visual-cards">
+                <div className="md:grid md:grid-cols-3 flex flex-col md:gap-6 gap-20 items-center visual-cards ">
                   <div className="md:w-[408px] w-full h-[200px] md:h-[408px] ">
-                    <img src={VisualImg} alt="" className="w-full h-[208px] flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
+                    <img src={VisualImg} alt="" className="w-full object-cover  h-[250px]  flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
                   </div>
                   <div className="md:w-[408px] w-full h-[200px] md:h-[408px]">
-                    <img src={VisualImg2} alt="" className="w-full h-[208px] flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
+                    <img src={VisualImg2} alt="" className="w-full object-cover  h-[250px]  flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
                   </div>
                   <div className="md:w-[408px] w-full h-[200px] md:h-[408px]">
-                    <img src={VisualImg3} alt="" className="w-full h-[208px] flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
+                    <img src={VisualImg3} alt="" className="w-full object-cover  h-[250px]  flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
                   </div>
                   <div className="md:w-[408px] w-full h-[200px] md:h-[408px]">
-                    <img src={VisualImg4} alt="" className="w-full h-[208px] flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
+                    <img src={VisualImg4} alt="" className="w-full object-cover  h-[250px]  flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
                   </div>
                   <div className="md:w-[408px] w-full h-[200px] md:h-[408px]">
-                    <img src={VisualImg5} alt="" className="w-full h-[208px] flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
+                    <img src={VisualImg5} alt="" className="w-full object-cover  h-[250px]  flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
                   </div>
                   <div className="md:w-[408px] w-full h-[200px] md:h-[408px]">
-                    <img src={VisualImg6} alt="" className="w-full h-[208px] flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
+                    <img src={VisualImg6} alt="" className="w-full object-cover  h-[250px]  flex rounded md:w-[408px] md:h-[408px] visual-cards-card " />
                   </div>
                 </div>
             </div>
