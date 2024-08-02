e0f73194ba0a098991b3c7bb3e6c40eed49b9825
umarali7270 <umaraliismoilov3101@gmail.com>
2024-08-02 10:48:17 +0500

    as

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -46,12 +46,12 @@ export default function Header2() {
 
             </div>
         </header>
-                <div style={xox ? {right: "-100%"} : {right: "0"}} onClick={durbek} className="w-[100%] transition-all bg-red-500 top-0 right-0 fixed h-screen ">
+                <div style={xox ?  {right: "0"} :{right: "-100%"}} onClick={durbek} className="w-[100%] transition-all bg-transparent top-0 right-0 fixed h-screen ">
                     
                 </div>
 
                 
-                <div style={nav ? {right: "-100%"} : {right: "0"}} className="md:hidden fixed top-0 pt-10   bg-[#202020a0]  z-50 backdrop-blur-md transition-all right-0 block h-[100vh] w-[85%]">
+                <div style={nav ?  {right: "0"} :{right: "-100%"}} className="md:hidden fixed top-0 pt-10   bg-[#202020a0]  z-50 backdrop-blur-md transition-all right-0 block h-[100vh] w-[85%]">
                     <button  onClick={durbek}  className=" absolute top-6 right-0 text-5xl flex justify-end pr-5">
                     <svg className='w-4 hamburger-x' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
                     </button>
